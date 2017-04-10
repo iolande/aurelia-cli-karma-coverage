97c4882326ee2e991d15936175ada3e36f910269
io <iolande@users.noreply.github.com>
2017-04-10 17:57:32 +0100

    Add some untested code
    To check coverage is working

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+
+  // some extra code to check the coverage task is working :)
+  doSomething(params = {}) {
+    if (!!params.id) {
+      return true;
+    }
+
+    return false;
+  }
 }
